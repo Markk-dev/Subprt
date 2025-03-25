390ea175676b952b9870aa91753a9c400beb7c7a
Markk-dev <markvincent.dev@gmail.com>
2025-03-25 09:35:11 +0800

    Add project files.

---
+++ Dailee_Konnekt/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dailee_Konnekt.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        public string ErrorMessage { get; set; }
+
+
+        public IActionResult OnPost()
+        {
+            if (Username == "Username" && Password == "Password")
+            {
+                return RedirectToPage("/Home");
+            }
+            else
+            {
+                ErrorMessage = "Invalid username or password.";
+                return Page();
+            }
+        }
+    }
+}
